92f1c64670cfa730c14321a6baa7703a1e23459a
Stefan Rosenlund <Stefan.Rosenlund@gmx.de>
2022-03-21 17:17:14 +0100

    Set Java system properties by using Ant properties

--- src/main/java/de/stro18/peass_ant/executor/TomcatCommandConstructor.java
+++ src/main/java/de/stro18/peass_ant/executor/TomcatCommandConstructor.java
@@ -1,5 +1,7 @@
 package de.stro18.peass_ant.executor;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class TomcatCommandConstructor implements AntCommandConstructor {
     
     private final String ant = "ant";
@@ -12,7 +14,15 @@ public class TomcatCommandConstructor implements AntCommandConstructor {
         return new String[] { ant, "clean" };
     }
     
-    public String[] constructTestExec(String testClass) {
-        return new String[] { ant, "test", "-Dexecute.test.nio2=false", "-Dexecute.test.apr=false", "-Dtest.entry=" + testClass};
+    public String[] constructTestExec(String testClass, String[] argLine) {
+        String[] command = new String[] { ant, "test", "-Dexecute.test.nio2=false", "-Dexecute.test.apr=false", "-Dtest.entry=" + testClass};
+        
+        StringBuilder stringBuilder = new StringBuilder(StringUtils.join(command, " "));
+        for (String arg : argLine) {
+            String[] keyAndValue = arg.split("=");
+            stringBuilder.append(" " + keyAndValue[0] + "=" + keyAndValue[1]);
+        }
+        
+        return stringBuilder.toString().split(" ");
     }
 }
